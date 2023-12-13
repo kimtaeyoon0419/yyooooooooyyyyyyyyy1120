5fc95532fede67585c6b5ed9606c170bee61d57a
part2170 <mpart2077@gmail.com>
2023-12-13 21:45:11 +0900

    fix2

--- Assets/Ws/Script/VolumeSetting.cs
+++ Assets/Ws/Script/VolumeSetting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class VolumeSetting : SingTon<VolumeSetting>
+public class VolumeSetting : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider MusicSlider;
